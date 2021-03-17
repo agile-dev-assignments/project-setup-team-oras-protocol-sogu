b958b1c27981525dbdab6ac10d76a53f901a371d
Michael Perillo <msp501@nyu.edu>
2021-03-17 15:10:11 -0400

    Initial Commit for Dashboard

---
+++ stonks-only-go-up/src/Components/Dashboard/DasboardMain.js
@@ -0,0 +1,7 @@
+import { React } from "react"
+
+// DashboardMain.js
+/*
+    Encapsulates main functionality of landing dashboard for users who have logged in. May need to rework file structure
+*/
+
